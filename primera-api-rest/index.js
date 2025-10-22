6d69e443e63b4eafa804229b328e2b7e8aff6e9c
Ita <itaplata.n@gmail.com>
2025-10-22 17:13:36 +0200

    primer commit api rest
    Primera parte api rest

--- primera-api-rest/index.js
+++ primera-api-rest/index.js
@@ -1,22 +1,21 @@
-//ME TRAIGO EL MÓDULO Y LO CONFIGURO
 require("dotenv").config();
 
 // nos traemos el módulo express que previamente hemos instalado
 const express = require("express");
 
-// lo ejecutamos y guardamos
-//  en la variable app
+// lo ejecutamos y guardamos en la variable app
 const app = express();
-
-// todas las rutas que no tengan respuesta entrarán por aquí
-app.use("*", (req, res, next) => {
-    return res.status(404).json("Route not found")
-})
-
 
 /* utilizamos nuestro servidor para ponerlo a escuchar con el método listen, le tenemos que pasar un puerto
  en el primer parámetro y un callback con la función a ejecutar cuando se ponga a escuchar.*/
+
+ // todas las rutas que no tengan respuesta entrarán por aquí
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+
+
 app.listen(3000, () => {
     console.log("http://localhost:3000");
 })
-
